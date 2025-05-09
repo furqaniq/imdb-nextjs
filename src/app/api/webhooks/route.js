b56c70dec2f4d15b043ef2c9638cbaac8023629e
furqaniq <furqan93@gmail.com>
2025-05-09 05:56:28 +0500

    add mongodb to connect clerk and database

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -20,8 +20,8 @@
 
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-// import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
-// import { clerkClient } from "@clerk/nextjs/server";
+import { createOrUpdateUser, deleteUser } from "@/lib/actions/user";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
@@ -72,61 +72,61 @@ export async function POST(req) {
   // For this guide, log payload to console
   const { id } = evt?.data;
   const eventType = evt?.type;
-  console.log(`Recieved webhook with ID ${id} and event type of ${eventType}`);
-  console.log(`Webhook payload:`, body);
+  //   console.log(`Recieved webhook with ID ${id} and event type of ${eventType}`);
+  //   console.log(`Webhook payload:`, body);
 
-  if (eventType === "user.created") {
-    console.log(`User created event`);
-  }
+  //   if (eventType === "user.created") {
+  //     console.log(`User created event`);
+  //   }
 
-  if (eventType === "user.updated") {
-    console.log(`User created event`);
-  }
-
-  if (eventType === "user.deleted") {
-    console.log(`User created event`);
-  }
-
-  //   if (eventType === "user.created" || eventType === "user.updated") {
-  //     const { first_name, last_name, image_url, email_addresses } = evt?.data;
-  //     try {
-  //       const user = await createOrUpdateUser(
-  //         id,
-  //         first_name,
-  //         last_name,
-  //         image_url,
-  //         email_addresses
-  //       );
-  //       if (user && eventType === "user.created") {
-  //         try {
-  //           const client = await clerkClient();
-  //           await client.users.updateUserMetadata(id, {
-  //             publicMetadata: {
-  //               userMongoId: user._id,
-  //             },
-  //           });
-  //         } catch (error) {
-  //           console.log("Error: Could not update user metadata:", error);
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.log("Error: Could not create or update user:", error);
-  //       return new Response("Error: Could not create or update user", {
-  //         status: 400,
-  //       });
-  //     }
+  //   if (eventType === "user.updated") {
+  //     console.log(`User created event`);
   //   }
 
   //   if (eventType === "user.deleted") {
-  //     try {
-  //       await deleteUser(id);
-  //     } catch (error) {
-  //       console.log("Error: Could not delete user:", error);
-  //       return new Response("Error: Could not delete user", {
-  //         status: 400,
-  //       });
-  //     }
+  //     console.log(`User created event`);
   //   }
+
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { first_name, last_name, image_url, email_addresses } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses
+      );
+      if (user && eventType === "user.created") {
+        try {
+          const client = await clerkClient();
+          await client.users.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user._id,
+            },
+          });
+        } catch (error) {
+          console.log("Error: Could not update user metadata:", error);
+        }
+      }
+    } catch (error) {
+      console.log("Error: Could not create or update user:", error);
+      return new Response("Error: Could not create or update user", {
+        status: 400,
+      });
+    }
+  }
+
+  if (eventType === "user.deleted") {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log("Error: Could not delete user:", error);
+      return new Response("Error: Could not delete user", {
+        status: 400,
+      });
+    }
+  }
 
   return new Response("Webhook received", { status: 200 });
 }
